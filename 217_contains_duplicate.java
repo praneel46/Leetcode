8fd39fd8742e402bb22820c11ea61d9ca5128092
Praneel C Kulkarni <praneelkulkarni4646@gmail.com>
2026-09-26 23:00:26 +0530

    Add 217_contains_duplicate.java - LeetCode Problem 217

---
+++ 217_contains_duplicate.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public boolean containsDuplicate(int[] nums) 
+    {
+        int n = nums.length;
+        Arrays.sort(nums);
+        if(n==1)
+        {
+            return false;
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return true;
+                
+            }
+        }
+                return false;
+    }
+}
