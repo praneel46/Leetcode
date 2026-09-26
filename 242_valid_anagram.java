c6226b7cee537b67ad0d70675de861cc66297e3f
Praneel C Kulkarni <praneelkulkarni4646@gmail.com>
2026-09-26 23:59:22 +0530

    Update 242_valid_anagram.java - LeetCode Problem 242

--- 242_valid_anagram.java
+++ 242_valid_anagram.java
@@ -1,18 +1,15 @@
-class Solution {
-    public boolean isAnagram(String s, String t) 
-    {
-        char[] arr = s.toCharArray();
-        char[] arr1 = t.toCharArray();
-        for(int i=0;i<arr.length;i++)
+class Solution
+ {
+    public boolean isAnagram(String s, String t)
+     {
+        if (s.length() != t.length())
         {
-            for(int j=0;j<arr1.length;j++)
-            {
-                if(arr[i]!=arr1[j])
-                {
-                    return false;
-                }
-            }
+            return false;
         }
-        return true ;
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
+        return Arrays.equals(arrS, arrT);
     }
 }
